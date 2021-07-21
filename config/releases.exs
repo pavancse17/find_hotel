1697564aef1505ddb861ba9ef812549b3e9e6954
Pavan Kumar <pavankumar.kuppala@tyson.com>
2021-07-21 14:32:58 +0530

    Remove compile time config

--- config/releases.exs
+++ config/releases.exs
@@ -26,7 +26,6 @@ secret_key_base =
 config :find_hotel, FindHotelWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "dry-hollows-59076.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
