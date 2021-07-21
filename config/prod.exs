e6fce42cdc97196cad753bf7bcd5b683adc5e60c
Pavan Kumar <pavankumar.kuppala@tyson.com>
2021-07-21 18:01:20 +0530

    Use normal deployment

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,33 @@ use Mix.Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :find_hotel, FindHotel.Repo,
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :find_hotel, FindHotelWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
+  load_from_system_env: true,
+  url: [scheme: "https", host: "stormy-hamlet-53760.herokuapp.com/", port: 443],
+  secret_key_base: secret_key_base
+
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
--- config/releases.exs
+++ config/releases.exs
@@ -22,11 +22,11 @@ secret_key_base =
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
     """
-IO.inspect(System.get_env("PORT"))
+
 config :find_hotel, FindHotelWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   load_from_system_env: true,
-  url: [scheme: "https", host: "thawing-journey-59645.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "stormy-hamlet-53760.herokuapp.com/", port: 443],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
